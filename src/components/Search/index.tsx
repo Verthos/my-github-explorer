122f0481380962e00a4d5e947ca963f580e4ac69
unknown <wpkszan@gmail.com>
2021-09-10 22:28:47 -0300

    switch button refactored. Functions that deals with selected filter, moved to the useSearch Rook

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -7,24 +7,9 @@ import { SearchBar, SwitchButton, Container} from "./styles";
 
 export function Search() {
     
-    const userButtomIsSelected = function (usersIsSelected: string){
-        if(usersIsSelected === "notSelected") {
-            setUsersIsSelected("selected")
-            setRepositoriesIsSelected("notSelected")
-        }else return;
-    }
-
-    const repositoriesButtomIsSelected = function (repositoriesIsSelected: string){
-        if(repositoriesIsSelected === "notSelected") {
-            setRepositoriesIsSelected("selected")
-            setUsersIsSelected("notSelected")
-        }else return;
-    }
 
     const search = useSearch();
     const [content, setContent] = useState("");
-    const [repositoriesIsSelected, setRepositoriesIsSelected] = useState("selected")
-    const [usersIsSelected, setUsersIsSelected] = useState("notSelected")
 
     
     
@@ -33,11 +18,11 @@ export function Search() {
             //entender a propriedade Key neste caso
         <Container>
 
-            <SwitchButton className={usersIsSelected} type="button" 
-            onClick={() => {search.handleQueryType("users"); userButtomIsSelected(usersIsSelected)} }>Users</SwitchButton>
+            <SwitchButton className={search.usersIsSelected} type="button" 
+            onClick={() => {search.userButtomIsSelected(search.usersIsSelected)} }>Users</SwitchButton>
 
-            <SwitchButton className={repositoriesIsSelected} type="button" 
-            onClick={() => {search.handleQueryType("repositories"); repositoriesButtomIsSelected(repositoriesIsSelected)}}>Repositories</SwitchButton>
+            <SwitchButton className={search.repositoriesIsSelected} type="button" 
+            onClick={() => {search.repositoriesButtomIsSelected(search.repositoriesIsSelected)}}>Repositories</SwitchButton>
             
             <SearchBar key={search.argument}>
                 <input value={content} onChange={event => setContent(event.target.value)} type="text" placeholder="Digite aqui." />
--- src/hooks/useSearch.tsx
+++ src/hooks/useSearch.tsx
@@ -3,11 +3,16 @@ import { createContext, useState, ReactNode, useContext } from "react";
 interface SearchProviderProps {
     children: ReactNode
 }
+
 interface SearchProviderData{
     queryType: string;
     argument: string;
+    usersIsSelected: string;
+    repositoriesIsSelected: string;
     handleNewArgument: (argument: string) => void;
-    handleQueryType: (type: string) => void;
+    userButtomIsSelected: (isSelected: string) => void;
+    repositoriesButtomIsSelected: (isSelected: string) => void;
+
 }
 
 
@@ -15,22 +20,38 @@ const SearchContext = createContext<SearchProviderData>({} as SearchProviderData
 
 export function SearchProvider({children} : SearchProviderProps) {
     
+    
+    const [repositoriesIsSelected, setRepositoriesIsSelected] = useState("selected")
+    const [usersIsSelected, setUsersIsSelected] = useState("notSelected")
     const [argument, setArgument] = useState("Verthos");
     const [queryType, setQueryType] = useState("repositories")
-
-    function handleQueryType(type: string){
-        setQueryType(type);
-        console.log(queryType)
-    }
 
 
     function handleNewArgument(argument: string){
         setArgument(argument)
     }
 
+    const userButtomIsSelected = function (usersIsSelected: string){
+        if(usersIsSelected === "notSelected") {
+            setUsersIsSelected("selected")
+            setRepositoriesIsSelected("notSelected")
+            setQueryType("users")
+        }else return;
+    }
+
+    const repositoriesButtomIsSelected = function (repositoriesIsSelected: string){
+        if(repositoriesIsSelected === "notSelected") {
+            setRepositoriesIsSelected("selected")
+            setUsersIsSelected("notSelected")
+            setQueryType("repositories")
+        }else return;
+    }
+
+
+
     return(
         
-        <SearchContext.Provider value={{handleNewArgument, handleQueryType, argument, queryType}}>
+        <SearchContext.Provider value={{handleNewArgument, argument, queryType, usersIsSelected, repositoriesIsSelected, userButtomIsSelected, repositoriesButtomIsSelected}}>
             {children}
         </SearchContext.Provider>
     )
